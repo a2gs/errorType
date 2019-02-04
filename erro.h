5e49f99d31ff80a1ee6ae9300dc8272477f86848
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-02-04 21:49:12 -0200

    Update erro.h

--- erro.h
+++ erro.h
@@ -1,45 +1,63 @@
+/* Andre Augusto Giannotti Scota (a2gs)               
+ * andre.scota@gmail.com
+ *
+ * A function error type
+ *
+ * Public Domain
+ *
+ */
+
+/* <file name>
+ * <File description>
+ *
+ *  Who     | When       | What
+ *  --------+------------+----------------------------
+ *   a2gs   | 01/01/2002 | Creation
+ *          |            |
+ */
+
 #ifndef __A2GS_TOOLBOX_ERRO_H__
 #define __A2GS_TOOLBOX_ERRO_H__
 
 
 /* *** INCLUDES *** */
-	#include <string.h>
+#include <string.h>
 
 
 /* *** DEFINES *** */
-	#define A2GS_TOOLBOX_ERRO_STR_SIZE 256
+#define A2GS_TOOLBOX_ERRO_STR_SIZE 256
 
-	/* Clear de erro_t variable */
-   #define A2GS_TOOLBOX_GET_ERRO(__err_)       (__err_.errno)
+/* Clear de erro_t variable */
+#define A2GS_TOOLBOX_GET_ERRO(__err_)       (__err_.errno)
 
-	/* Return the erro's number defined by someone into a erro_t variable */
-   #define A2GS_TOOLBOX_GET_ERROSTRING(__err_) (__err_.errstr)
+/* Return the erro's number defined by someone into a erro_t variable */
+#define A2GS_TOOLBOX_GET_ERROSTRING(__err_) (__err_.errstr)
 
-	/* Return the erro's message defined by someone into a erro_t variable */
-	#define A2GS_TOOLBOX_CLEAR_ERRO(__err_){                                                          \
-                                        __err_.errno = 0;                                            \
-	                                     memset(__err_.errstr, '\0', A2GS_TOOLBOX_ERRO_STR_SIZE + 1); \
-                                      }
+/* Return the erro's message defined by someone into a erro_t variable */
+#define A2GS_TOOLBOX_CLEAR_ERRO(__err_){                                                             \
+                                       	__err_.errno = 0;                                            \
+                                       	memset(__err_.errstr, '\0', A2GS_TOOLBOX_ERRO_STR_SIZE + 1); \
+                                       }
 
-	/* Define a erro into a erro_t variable
-	 * __err_        - erro_t variable
-	 * __errno_      - error number
-	 * __errstr_     - error message
-	 * __errstrSize_ - Size of __errstr_ in bytes (max size can be A2GS_TOOLBOX_ERRO_STR_SIZE)
-	 */
-	#define A2GS_TOOLBOX_SET_ERRO(__err_, __errno_, __errstr_, __errstrSize_){                                                  \
-                             __err_.errno = __errno_;                                                                          \
-                             memcpy(__err_.errstr, __errstr_,                                                                  \
-	                                (A2GS_TOOLBOX_ERRO_STR_SIZE < __errstrSize_ ? A2GS_TOOLBOX_ERRO_STR_SIZE : __errstrSize_)); \
-	                          __err_.errstr[(A2GS_TOOLBOX_ERRO_STR_SIZE < __errstrSize_ ? A2GS_TOOLBOX_ERRO_STR_SIZE : __errstrSize_)] = '\0'; \
-	                                                                         }
+/* Define a erro into a erro_t variable
+ * __err_        - erro_t variable
+ * __errno_      - error number
+ * __errstr_     - error message
+ * __errstrSize_ - Size of __errstr_ in bytes (max size can be A2GS_TOOLBOX_ERRO_STR_SIZE)
+ */
+#define A2GS_TOOLBOX_SET_ERRO(__err_, __errno_, __errstr_, __errstrSize_){                                                                       \
+                              	__err_.errno = __errno_;                                                                                         \
+                              	memcpy(__err_.errstr, __errstr_,                                                                                 \
+                              	(A2GS_TOOLBOX_ERRO_STR_SIZE < __errstrSize_ ? A2GS_TOOLBOX_ERRO_STR_SIZE : __errstrSize_));                      \
+                              	__err_.errstr[(A2GS_TOOLBOX_ERRO_STR_SIZE < __errstrSize_ ? A2GS_TOOLBOX_ERRO_STR_SIZE : __errstrSize_)] = '\0'; \
+                              }
 
 
 /* *** DATA TYPES *** */
-	typedef struct _erro_t{
-		int errno;
-		char errstr[A2GS_TOOLBOX_ERRO_STR_SIZE + 1];
-	}erro_t;
+typedef struct _erro_t{
+	int errno;
+	char errstr[A2GS_TOOLBOX_ERRO_STR_SIZE + 1];
+}erro_t;
 
 
 /* *** INTERFACES / PROTOTYPES *** */
@@ -47,16 +65,16 @@
 
 /* *** EXAMPLE *** */
 #if 0
-	int main(){
-		erro_t e;
-		char *msg = "erro 13";
+int main(){
+	erro_t e;
+	char *msg = "erro 13";
 
-		A2GS_TOOLBOX_CLEAR_ERRO(e);
+	A2GS_TOOLBOX_CLEAR_ERRO(e);
 
-		A2GS_TOOLBOX_SET_ERRO(e, 13, msg, strlen(msg));
+	A2GS_TOOLBOX_SET_ERRO(e, 13, msg, strlen(msg));
 
-		printf("[%d][%s]\n", A2GS_TOOLBOX_GET_ERRO(e), A2GS_TOOLBOX_GET_ERROSTRING(e));
-	}
+	printf("[%d][%s]\n", A2GS_TOOLBOX_GET_ERRO(e), A2GS_TOOLBOX_GET_ERROSTRING(e));
+}
 #endif
 
 #endif
